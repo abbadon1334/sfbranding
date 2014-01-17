1145d3f05008347889329cb7c241daf76c93de7f
Tom <tom@thefrontdoor.nl>
2014-01-17 16:22:48 +0100

    Updated branding to reflect architecture changes in brandingbase + sf client

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,13 +1,8 @@
 pref("app.releaseNotesURL", "http://www.sitefusion.org/releasenotes");
 pref("app.update.url", "https://www.sitefusion.org/-1/phpblock/update.xml?vendor=SiteFusion.org&product=%PRODUCT%&version=%VERSION%&buildid=%BUILD_ID%&buildtarget=%BUILD_TARGET%&locale=%LOCALE%&channel=%CHANNEL%&osversion=%OS_VERSION%&distribution=%DISTRIBUTION%&distributionversion=%DISTRIBUTION_VERSION%");
 
-pref("browser.preferences.instantApply", false);
-
 pref("sitefusion.defaultLoginWindowURI","chrome://sforg/content/login.xul");
 pref("sitefusion.defaultRootWindowURI","chrome://sitefusion/content/rootwindow.xul");
 pref("sitefusion.defaultChildWindowURI","chrome://sitefusion/content/childwindow.xul");
 pref("sitefusion.defaultPrefWindowURI","chrome://sitefusion/content/prefwindow.xul");
 pref("sitefusion.defaultDialogURI","chrome://sitefusion/content/dialog.xul");
-
-pref("breakpad.reportURL","http://crash-stats.mozilla.com/report/index/");
-pref("app.support.baseURL","http://www.sitefusion.org/");
