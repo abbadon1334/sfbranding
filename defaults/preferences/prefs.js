71d1745e33cef39a8344702e87af2cd0b9eaa1fc
Tom <tom@thefrontdoor.nl>
2014-01-17 17:04:12 +0100

    Updated about.xhtml and prefs.js

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -6,3 +6,4 @@ pref("sitefusion.defaultRootWindowURI","chrome://sitefusion/content/rootwindow.x
 pref("sitefusion.defaultChildWindowURI","chrome://sitefusion/content/childwindow.xul");
 pref("sitefusion.defaultPrefWindowURI","chrome://sitefusion/content/prefwindow.xul");
 pref("sitefusion.defaultDialogURI","chrome://sitefusion/content/dialog.xul");
+pref("app.support.baseURL","http://www.sitefusion.org/");
